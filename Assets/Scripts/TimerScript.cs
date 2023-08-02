87ffd9c978ed128d329046992062adf405b7676b
Abdullah <73937383+nemesisx18@users.noreply.github.com>
2023-08-03 05:46:26 +0700

    Change timer to countdown & add sfx - case 1

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -3,35 +3,41 @@ using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    public float timeLimit = 120; // Waktu total dalam detik
-    private float currentTime; // Waktu saat ini
+    //public float timeLimit = 120; // Waktu total dalam detik
+    public float currentTime; // Waktu saat ini
     public Text timerText; // Komponen Text untuk menampilkan timer
+
+    public bool timerIsRunning = true;
 
     private void Start()
     {
-        currentTime = 0; // Mengatur waktu saat ini menjadi 0
+        currentTime = 120; // Mengatur waktu saat ini menjadi 0
     }
 
     private void Update()
     {
-        // Menambah waktu saat ini dengan waktu yang telah berlalu
-        currentTime += Time.deltaTime;
+        if (timerIsRunning)
+        {
+            // Menambah waktu saat ini dengan waktu yang telah berlalu
+            currentTime -= Time.deltaTime;
 
-        // Mengubah waktu dalam detik menjadi menit dan detik terpisah
-        int minutes = Mathf.FloorToInt(currentTime / 60);
-        int seconds = Mathf.FloorToInt(currentTime % 60);
+            // Mengubah waktu dalam detik menjadi menit dan detik terpisah
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
 
-        // Format string untuk menampilkan waktu dengan angka menit di sebelah kiri, angka detik di sebelah kanan, dan ":" di tengahnya
-        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+            // Format string untuk menampilkan waktu dengan angka menit di sebelah kiri, angka detik di sebelah kanan, dan ":" di tengahnya
+            string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-        // Menampilkan waktu pada komponen Text
-        timerText.text = timerString;
+            // Menampilkan waktu pada komponen Text
+            timerText.text = timerString;
+        }
 
         // Jika waktu mencapai batas waktu, lakukan sesuatu di sini
-        if (currentTime >= timeLimit)
+        if (currentTime <= 0)
         {
-            // Lakukan sesuatu di sini
-            // Contoh: GameManager.instance.EndGame();
+            timerIsRunning = false;
+            string timerString = string.Format("{0:00}:{1:00}", 0, 0);
+            timerText.text = timerString;
         }
     }
 }
